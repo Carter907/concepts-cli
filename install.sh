e4baaf53b96cc96b97e36ec31c81d36f07e92c70
Carter <speers.carter@gmail.com>
2025-03-18 00:16:12 -0400

    wip: install script edits

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-install_dir='/usr/bin/concepts'
-mkdir -p $install_dir
-curl -s https://raw.githubusercontent.com/Carter907/concepts/main/concepts >>$install_dir'/concepts'
+install_dir="$HOME"'/bin'
+mkdir -p "$install_dir"
+curl -s https://raw.githubusercontent.com/Carter907/concepts/main/concepts >>"$install_dir"'/concepts'
 
 echo 'concepts installed at '"$install_dir"
